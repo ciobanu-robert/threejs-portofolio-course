2693f6aa535ecd9a219cbefa28dbc429cef788c6
Robi <ciobanurobert100@gmail.com>
2024-07-19 14:01:14 +0300

    added preloaders and loaders

--- src/App/app.js
+++ src/App/app.js
@@ -5,6 +5,8 @@ import Renderer from './renderer';
 import Loop from './Utils/loop';
 import World from '../World/world';
 import Resize from './Utils/resize';
+import AssetLoader from './Utils/asset.loader';
+import Preloader from './UI/preloader';
 
 let instance = null;
 
@@ -15,6 +17,8 @@ export default class App {
 
         this.canvas = document.querySelector("canvas.threejs");
         this.scene = new THREE.Scene();
+        this.assetLoader = new AssetLoader();
+        this.preloader = new Preloader();
         this.camera = new Camera();
         this.renderer = new Renderer();
         this.world = new World();
